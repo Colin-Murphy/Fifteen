88de874e82885433f482fce78ef19609469cfe57
Colin Murphy <ColinMurphy1993@gmail.com>
2014-05-28 15:51:22 -0400

    Finished project, forgot to commit

---
+++ ViewListener.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+
+/**
+	The interface for listening to a FifteenView object
+	@author Colin L Murphy <clm3888@rit.edu>
+	@version 4/12/14
+*/
+
+
+public interface ViewListener {
+
+	/**
+		Player joined the server
+		@param name players name
+		@param proxy The view proxy
+	*/
+	public void joined(String name);
+	
+	/**
+		Player clicked a digit
+		@param digit the digit clicked
+	*/
+	public void digit(int digit);
+
+
+	/**
+		Player has restarted the game
+	*/
+	public void newGame();
+	
+	
+	/**
+		Player quit the game
+	*/
+	public void quit();
+	
+}
